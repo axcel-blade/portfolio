72e0b10c8bb2bfa58f706e486e3906d01cd2c28e
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-07 13:45:11 +0800

    Refactor Header component and update Home page layout

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,6 @@
 export default function Header() {
     return (
-        <header 
-        className='bg-black'
-        >
+        <header>
             <div 
             className="flex justify-center"
             >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-import Header from "./components/Header";
+import Header from "./components/header";
+import Particles from "./components/particles";
 
 export default function Home() {
   return (
     <div>
-      {/*Header*/}
-      <Header/>
-      <main>
+      {/*Main*/}
+      <main 
+      className="flex flex-col items-center justify-center h-screen text-center"
+      >
+        {/*Header*/}
+        <Header/>
+        {/*Background Particles*/}
+        <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+        />
+        <div>
+          <span 
+          className="text-8xl"
+          >
+            Joseph Fernando
+          </span>
+          <br />
+          <br />
+          <span
+          className="text-4xl"
+          >
+            Software Engineer
+          </span>
+        </div>
       </main>
     </div>
   );
