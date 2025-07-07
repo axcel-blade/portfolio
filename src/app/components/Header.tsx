d03c471747310a7f73b7d134b131bfe5366aeffe
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-07 13:57:30 +0800

    Update Header component styles: add fixed position and z-index

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,13 +2,13 @@ export default function Header() {
     return (
         <header>
             <div 
-            className="flex justify-center"
+            className="flex justify-center position: fixed top-0 left-0 right-0 z-10"
             >
                 {/* This is hover underline}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 */}
                 <a
-                className="px-8 py-8 text-white hover:text-grey-500"
+                className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 >
                     Home
                 </a>
