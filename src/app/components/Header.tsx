0ed2c551d527c1e91ef20b3194ce87c33f6ddd08
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-07 13:53:01 +0800

    Removed hover underline effect from Header component and removed unnecessary import in particles component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,8 +4,11 @@ export default function Header() {
             <div 
             className="flex justify-center"
             >
-                <a
+                {/* This is hover underline}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                */}
+                <a
+                className="px-8 py-8 text-white hover:text-grey-500"
                 >
                     Home
                 </a>
