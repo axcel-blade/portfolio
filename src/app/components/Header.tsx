fa44bfecdb1b054b767c0e1294fee40725cb96d1
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-07 13:14:16 +0800

    Added dependencies and updated Header.tsx

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,39 @@
+export default function Header() {
+    return (
+        <header 
+        className='bg-black'
+        >
+            <nav>
+                <div 
+                className="flex justify-center"
+                >
+                    <a
+                    className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                    >
+                        Home
+                    </a>
+                    <a 
+                    className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                    >
+                        About
+                    </a>
+                    <a 
+                    className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                    >
+                        Experience
+                    </a>
+                    <a 
+                    className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                    >
+                        Projects
+                    </a>
+                    <a 
+                    className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+                    >
+                        Contact
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
