fafda7932e6216008e5f3c7fb1c9a3b475ff7a68
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-07 14:38:42 +0800

    Added Link component from 'next/link' to replace a tags in Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Header() {
     return (
         <header>
@@ -7,31 +9,36 @@ export default function Header() {
                 {/* This is hover underline}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 */}
-                <a
+                <Link
+                href={"#"}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 >
                     Home
-                </a>
-                <a 
+                </Link>
+                <Link
+                href={"#"}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 >
                     About
-                </a>
-                <a 
+                </Link>
+                <Link
+                href={"#"}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 >
                     Experience
-                </a>
-                <a 
+                </Link>
+                <Link
+                href={"#"}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 >
                     Projects
-                </a>
-                <a 
+                </Link>
+                <Link
+                href={""}
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 >
                     Contact
-                </a>
+                </Link>
             </div>
         </header>
     )
