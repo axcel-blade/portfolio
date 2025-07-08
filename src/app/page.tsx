d2a89677b5750b54368b1dfa42712458508bc6fe
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-08 10:31:43 +0800

    Remove unwanted div tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/app/components/header"
-import Particles from "@/app/components/particles"
+//import Particles from "@/app/components/particles"
 import MatrixRain from "@/app/components/MatrixRain"
 import { SocialIcon } from 'react-social-icons/component'
 import 'react-social-icons/github'
@@ -18,37 +18,35 @@ export default function Home() {
       
       {/*Body*/}
       <div  className="flex flex-col items-center justify-center h-screen text-center backdrop-blur-sm">
-        <div className="flex flex-col">
-          <div className="flex flex-col items-center">
-            <span  className="text-8xl">
-              Joseph Fernando
-            </span>
-            <br />
-            <br />
-            <span  className="text-4xl">
-              Software Engineer
-            </span><br />
-          </div>
+        <div className="flex flex-col items-center">
+          <span  className="text-8xl">
+            Joseph Fernando
+          </span>
+          <br />
+          <br />
+          <span  className="text-4xl">
+            Software Engineer
+          </span><br />
+        </div>
 
-          {/*Socials*/}
-          <div className="flex flex-row py-4 justify-center">
-              <div className="px-4">
-                <SocialIcon network="github" url="https://www.github.com/axcel-blade"/>
-              </div>
-              <div className="px-4">
-                <SocialIcon network="linkedin" url="https://www.linkedin.com/in/srikanth-fernando" />
-              </div>
-              <div className="px-4">
-                <SocialIcon network="email" url="mailto:srikanthfernando3@gmail.com" />
-              </div>
-          </div>
+        {/*Socials*/}
+        <div className="flex flex-row py-4 justify-center">
+            <div className="px-4">
+              <SocialIcon network="github" url="https://www.github.com/axcel-blade"/>
+            </div>
+            <div className="px-4">
+              <SocialIcon network="linkedin" url="https://www.linkedin.com/in/srikanth-fernando" />
+            </div>
+            <div className="px-4">
+              <SocialIcon network="email" url="mailto:srikanthfernando3@gmail.com" />
+            </div>
+        </div>
 
-          {/*Download CV*/}
-          <div className="py-4 items-center">
-            <a href="#" className="px-4 py-2 border border-white rounded-lg hover:bg-white hover:text-black">
-              Download CV
-            </a>
-          </div>
+        {/*Download CV*/}
+        <div className="py-4 items-center">
+          <a href="#" className="px-4 py-2 border border-white rounded-lg hover:bg-white hover:text-black">
+            Download CV
+          </a>
         </div>
       </div>
     </div>
