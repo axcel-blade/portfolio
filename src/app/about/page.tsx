c4bccb8e628a19a3f9f67f2bd8ebef0488781bb0
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-08 11:25:27 +0800

    Add about paragraph

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import Header from '@/app/components/header'
 import Particles from "@/app/components/particles"
+import MatrixRain from "@/app/components/MatrixRain"
 
 export default function About() {
     return (
         <div>
-            <main>
-                <Header/>
-                <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>
-                <h1>About</h1>
-            </main>
+            {/*Header*/}
+            <Header/>
+
+            {/*Background animation*/}
+            {/*<Particles  className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>*/}
+            <MatrixRain />
+
+            {/*Body*/}
+            <div className=" flex flex-col max-w-screen-xl mx-auto px-4 text-center justify-center h-screen backdrop-blur-sm">
+                <div>
+                    <span className='text-8xl'>
+                        About Me
+                    </span>
+                </div>
+                <div>
+                    <span className='text-2xl'>
+                        I am a passionate and versatile Software Engineering graduate from Curtin University, Perth, Australia. As a lifelong learner and explorer of technology, my journey has been shaped by a deep curiosity for systems, software, and real-world problem-solving.<br/>
+
+                        With over five years of hands-on experience in IT and technical support, I bring a solid foundation in hardware maintenance, software troubleshooting, and network configuration—skills I developed while working as a Computer Technician in Sri Lanka. My professional experience also spans delivery and hospitality roles, reinforcing my adaptability, reliability, and strong work ethic.<br/>
+
+                        My passion lies in crafting modern, scalable web applications using technologies like React.js, Next.js, Gatsby, and MongoDB. I enjoy bringing digital ideas to life through clean, maintainable code and thoughtful user experiences. Whether building static sites with Gatsby, server-rendered applications with Next.js, or dynamic interfaces with React, I aim to create solutions that are both elegant and impactful.<br/>
+
+                        In addition to my frontend and backend development skills, I’m proficient in Java, Python, C#, the Microsoft Office Suite, and WordPress. I approach each project with creativity, precision, and a commitment to continuous learning.<br/>
+
+                        Beyond the code, I believe in building technology that is inclusive, accessible, and meaningful. Every interaction matters—and I’m driven to create digital experiences that leave a positive mark.<br/>
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <header>
-            <div className="flex justify-center position: fixed top-0 left-0 right-0 z-10">
+            <div className="flex justify-center position: relative top-0 left-0 right-0 z-10 backdrop-blur-sm">
                 {/* This is hover underline
                 className="px-8 py-8 relative inline-block text-white cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full"
                 */}
