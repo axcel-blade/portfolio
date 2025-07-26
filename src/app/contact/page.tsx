c966f5f6b8740bfee32584d4cc29302c6b431f72
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-26 21:05:54 +0800

    Revert import change in Contact page and update Particles component to use Circle[] type

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
-{/*import Particles from '../components/Particles'*/}
-import MatrixRain from '../components/MatrixRain'
+import Particles from '../components/Particles'
+{/*import MatrixRain from '../components/MatrixRain'*/}
 
 
 export default function Contact() {
@@ -10,8 +10,8 @@ export default function Contact() {
             <main>
                 <Header/>
                 {/*Background animation*/}
-                {/*<Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>*/}
-                <MatrixRain/>
+                <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>
+                {/*<MatrixRain/>*/}
                 <h1>Contact</h1>
             </main>
         </div>
