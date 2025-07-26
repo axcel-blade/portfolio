a65c2edb8caab3fc50b5a405201d8b5a6ed091bd
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-26 20:34:45 +0800

    Update import statements to use PascalCase for component names

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../components/header'
+import Header from '../components/Header'
 {/*import Particles from "@/app/components/particles"*/}
 import MatrixRain from "../components/MatrixRain"
 import ExperienceTimeline from "../experience/ExperienceTimeline"
