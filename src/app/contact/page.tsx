0a860dd0d8578923ef3d9350aec6446d4424fac0
srikanth-fernando <srikanthfernando3@gmail.com>
2025-07-26 21:00:56 +0800

    Replace Particles with MatrixRain in Contact and Projects pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
-import Particles from '../components/Particles'
+{/*import Particles from '../components/Particles'*/}
+import MatrixRain from '../components/MatrixRain'
 
 
 export default function Contact() {
@@ -8,7 +9,9 @@ export default function Contact() {
         <div>
             <main>
                 <Header/>
-                <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>
+                {/*Background animation*/}
+                {/*<Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>*/}
+                <MatrixRain/>
                 <h1>Contact</h1>
             </main>
         </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
-import Particles from '../components/Particles'
+{/*import Particles from '../components/Particles'*/}
+import MatrixRain from '../components/MatrixRain'
 
 export default function Projects() {
     return (
         <div>
             <main>
                 <Header/>
-                <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>
+                {/*Background animation*/}
+                {/*<Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>*/}
+                <MatrixRain/>
                 <h1>Projects</h1>
             </main>
         </div>
